cfcf79983f4d19a652c1af075c0775259bdfede0
visaeva <visaeva0302@gmail.com>
2024-08-07 13:51:35 +0300

    Initial Commit

---
+++ testSystems/testSystemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  testSystemsApp.swift
+//  testSystems
+//
+//  Created by Victoria Isaeva on 07.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct testSystemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
