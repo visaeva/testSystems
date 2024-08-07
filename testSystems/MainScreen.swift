a765e65135b55b20f492cba896b57b4b2b819f46
visaeva <visaeva0302@gmail.com>
2024-08-07 14:47:04 +0300

    assets

---
+++ testSystems/MainScreen.swift
@@ -0,0 +1,18 @@
+//
+//  MainScreen.swift
+//  testSystems
+//
+//  Created by Victoria Isaeva on 07.08.2024.
+//
+
+import SwiftUI
+
+struct MainScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainScreen()
+}
